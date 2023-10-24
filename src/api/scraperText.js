1ecc9c816af2649324939df828c24a96ef5dbecb
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-24 14:36:33 -0700

    Api integration

---
+++ src/api/scraperText.js
@@ -0,0 +1,27 @@
+ async function scraperText(payload) {
+    let final_response
+    try {
+      const response =  await fetch('http://localhost:8000/api/v1/scraping', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
+      .then((response) => {
+        if (response.status==200) {
+          return response.json();
+        }
+      })
+      .then(data => {
+        console.log('API response:', data);
+        final_response = data
+        return data
+      })
+      return final_response;
+    } catch (error) {
+      console.error("Error fetching scraper text:", error);
+      return [];
+    }
+  }
+  export default scraperText;
