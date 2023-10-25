cda6509c0aa90937d953cdbb171c8e932208db36
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-24 20:57:07 -0700

    word cloud changes

--- src/components/homePage/keywordListFrame.js
+++ src/components/homePage/keywordListFrame.js
@@ -1,49 +1,63 @@
-import React from 'react';
-import {
-  Chart as ChartJS,
-  LinearScale,
-  PointElement,
-  Tooltip,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, LinearScale, PointElement, Tooltip, Legend } from 'chart.js';
 import { Bubble } from 'react-chartjs-2';
-import faker from 'faker';
+import faker from 'faker'
 
 ChartJS.register(LinearScale, PointElement, Tooltip, Legend);
 
-export const options = {
-  scales: {
-    y: {
-      beginAtZero: true,
+const KeywordBubbleChart = ({ keywordListData }) => {
+  const bubbleData = keywordListData.topKeywordListings.map(item => ({
+    r: item.count,
+    original:item.original
+    
+  }));
+
+  const data = {
+    datasets: [
+      {
+        label: 'Keyword Dataset',
+        data: bubbleData.map(item => ({
+            x: faker.datatype.number({ min: -100, max: 100 }),
+            y: faker.datatype.number({ min: -100, max: 100 }),
+            r: item.r,
+            
+          })),
+        backgroundColor: (context) => {
+          const count = context.dataset.data[context.dataIndex].r;
+          if (count > 65) {
+            return 'rgba(255, 255, 0, 0.5)'; // yellow
+          } else if (count > 35) {
+            return 'rgba(255, 165, 0, 0.5)'; // orange
+          } else {
+            return 'rgba(53, 162, 235, 0.5)'; // blue
+          }
+        },
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'linear',
+        position: 'bottom',
+      },
+      y: {
+        beginAtZero: true,
+      },
     },
-  },
+   
+    plugins: {
+        tooltip: {
+        callbacks: {
+          label: (context) => {
+            const original = bubbleData[context.dataIndex].original;
+            return `Original: ${original}`;
+          },
+        },
+      },
+      },
+    };
+
+ return <Bubble options={options} data={data} style={{margin:'6%'}} />;
 };
-
-// sort into 2-3 colors for each group based on count (will be r here); count > 10 (oranges) and count > 15 (reds) and count < 10 (blues)
-
-export const data = {
-  datasets: [
-    {
-      label: 'Red dataset',
-      data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }), // leave these 
-        y: faker.datatype.number({ min: -100, max: 100 }), // leave these
-        r: faker.datatype.number({ min: 5, max: 20 }), // this will be count
-      })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Blue dataset',
-      data: Array.from({ length: 50 }, () => ({
-        x: faker.datatype.number({ min: -100, max: 100 }),
-        y: faker.datatype.number({ min: -100, max: 100 }),
-        r: faker.datatype.number({ min: 5, max: 20 }),
-      })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
-
-export default function keywordListFrame() {
-  return <Bubble options={options} data={data} />;
-}
+export default KeywordBubbleChart;
