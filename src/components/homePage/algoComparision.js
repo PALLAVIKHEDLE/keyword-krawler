0fde46ab14eab2a44ffef890d4e6c1f7bbb2b748
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-24 20:56:56 -0700

    graph changes

--- src/components/homePage/algoComparision.js
+++ src/components/homePage/algoComparision.js
@@ -12,40 +12,44 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  indexAxis: 'y',
-  elements: {
-    bar: {
-      borderWidth: 2,
+const AlgoComparision = ({ multialgo }) => {
+  const algoData = multialgo.data.map(item => ({
+    algoName: item.algoName,
+    algoExecutionTime: item.algoExecutionTime,
+  }));
+
+  const data = {
+    labels: algoData.map(item => item.algoName),
+    datasets: [
+      {
+        label: 'Execution Time',
+        data: algoData.map(item => item.algoExecutionTime),
+        borderColor: 'white',
+        backgroundColor: 'rgba(255, 255, 0, 0.5)',
+      },
+    ],
+  };
+
+  const options = {
+    indexAxis: 'y',
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
     },
-  },
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'right',
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'right',
+      },
+      title: {
+        display: true,
+        text: 'String Algorithm Comparison',
+      },
     },
-    title: {
-      display: true,
-      text: 'Chart.js Horizontal Bar Chart',
-    },
-  },
+  };
+
+  return <Bar options={options} data={data} style={{ margin: '10%' }} />;
 };
 
-const labels = ['Robin Karp ', 'Knuth Morris', 'Naive', 'Boyre Moore'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-   
-  ],
-};
-
-export  default function AlgoComparision() {
-  return <Bar options={options} data={data} />;
-}
+export default AlgoComparision;
