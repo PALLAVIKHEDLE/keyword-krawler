5a484a24a13b1a3174156d15c17f5d15a5e7e7ea
PallaviKhedle <pallavi8khedle@gmail.com>
2023-10-26 21:16:33 -0700

    Description & style changes

--- src/components/homePage/scraperTextFrame.js
+++ src/components/homePage/scraperTextFrame.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './scraperFrame.css';
+import { Card, CardContent } from '@mui/material';
+
 
 function ScraperTextFrame(props) {
+  console.log('props', props);
   return (
     <div className='scrapercontainer'>
-      <h1 className='h1textStyle'>Scraper context for {props.url}</h1>
-      <div className='scraperData'>
-        <p>{props.scraperData?.scrapedContent}</p>
-      </div>
+    <div className='h1container'>
+      <h1 className='h1textStyle'>Scrapper context for </h1>
+     {props&&<h2 className='urlText'>{props?.url}</h2>} 
     </div>
+
+    {props.analyzerData&&props?.analyzerData.pages[0]?.description && 
+      <Card className='cardStyle'>
+        <CardContent className='cardContentStyle'>
+          Description: {props?.analyzerData.pages[0].description}
+        </CardContent>
+      </Card>
+    }
+   
+
+    <div className='scraperData'>
+      <p>{props?.scraperData?.scrapedContent}</p>
+    </div>
+  </div>
   );
 }
 export default ScraperTextFrame;
